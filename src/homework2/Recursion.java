e708f6dcabfa1d17bac6574829f5aeaeab711b46
Kwankaew Uttama <kwankaew.u@ku.th>
2018-03-21 20:34:28 +0700

    done problem3

--- src/homework2/Recursion.java
+++ src/homework2/Recursion.java
@@ -6,23 +6,26 @@ import java.util.List;
 public class Recursion {
 	/**
 	 * Remove duplicate consecutive items from a list, changing the list parameter.
-	 * For example, if list = { a, b, b, b, c, b, c, c} then after calling unique the list
-	 * will contain {a, b, c, b, c}. Only consecutive duplicates are removed.
-	 * Objects are compared using their own equals method.
-	 * @param list of any kind of object. The elements are not null.
+	 * For example, if list = { a, b, b, b, c, b, c, c} then after calling unique
+	 * the list will contain {a, b, c, b, c}. Only consecutive duplicates are
+	 * removed. Objects are compared using their own equals method.
+	 * 
+	 * @param list
+	 *            of any kind of object. The elements are not null.
 	 * @return reference to the list parameter with consecutive duplicates removed.
 	 */
-	public static List<String> unique(List<String> list){
-		if( list.size() == 1 ) return list;
-		if( list.get(0).equals(list.get(1))) {
-			//System.out.println(list.get(0) +" "+list.get(1));
-			return unique(list.subList(1,list.size()));
-		}
-		List<String> alpha = list.subList(0,1);
-		alpha.addAll(unique(list.subList(1,list.size())));
-		return alpha;
+	public static List<?> unique(List<?> list) {
+		if (list.size() == 1)
+			return list;
+		List<?> line = list.subList(1, list.size());
+		if (list.get(0).equals(list.get(1))) {
+			line.remove(0);
+			unique(list);
+		} else
+			unique(line);
+		return list;
 	}
-	
+
 	public static void main(String[] args) {
 		List<String> list = new ArrayList<>();
 		list.add("a");
@@ -33,10 +36,8 @@ public class Recursion {
 		list.add("b");
 		list.add("c");
 		list.add("c");
-		//for(Object o: list) System.out.print(o+", "); //a, b, b, b, c, b, c, c, 
-		//System.out.println();
+		System.out.println(list);
 		System.out.println(unique(list));
-		//for(Object o: list) System.out.print(o+", ");  
-		
+
 	}
 }
